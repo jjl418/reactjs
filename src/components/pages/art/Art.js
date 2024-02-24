c6a1feb2720dc8ef8d0d990d5e7d046a4ec0eaff
julianne lee <jlee3932@usc.edu>
2024-02-24 01:51:41 -0800

    finished adding details for project 4
    finished adding details for project 4

--- src/components/pages/art/Art.js
+++ src/components/pages/art/Art.js
@@ -2,11 +2,27 @@ import '../../../App';
 import './Art.css';
 
 function Art() {
-    <>
+
+  
+
     return (
-
-    );
+    <>
+        <div className = "projects-background-2">
+          <div className = "sketchbook-tabs"> 
+            <div className = "art-tab">
+              <h1 className = "art-tab-caption">
+                2D-art
+              </h1>
+            </div>
+            <div className = "art-tab">
+              <h1 className = "art-tab-caption">
+                3D-art
+              </h1>
+            </div>
+          </div>
+        </div>
     </>
+    );
 }
-
+  
 export default Art;
--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -28,6 +28,8 @@ import sad_gif from '../../../images/project3_images/BB_sad.gif';
 import neutral_gif from '../../../images/project3_images/BB_neutral.gif';
 import p3_video from '../../../videos/BB_demo.mp4';
 import qr_image from '../../../images/project3_images/BB_QR.png';
+import p4_video from '../../../videos/Trivia-game_demo.mp4';
+
 import React, { useState } from 'react';
 
 
@@ -117,6 +119,28 @@ function Projects() {
     'UI Artist: Dorian Trinh',
     'UI Artist: Dorian Trinh',
   ];
+
+  //Project 4: Trivia-Game
+  const [currentIndex6, setCurrentIndex6] = useState(0); //index for p1_images list
+  const p4_images = [
+    require('../../../images/project4_images/Page1.png'),
+    require('../../../images/project4_images/Page2.png'),
+    require('../../../images/project4_images/Page3.png'),
+    require('../../../images/project4_images/Page4.png'),
+    require('../../../images/project4_images/Page5.png'),
+    require('../../../images/project4_images/Page6.png'),
+    require('../../../images/project4_images/Page7.png'),
+    require('../../../images/project4_images/Page8.png'),
+    require('../../../images/project4_images/Page9.png'),
+    require('../../../images/project4_images/Page10.png'),
+    require('../../../images/project4_images/Page11.png'),
+    require('../../../images/project4_images/Page12.png'),
+    require('../../../images/project4_images/Page13.png'),
+    require('../../../images/project4_images/Page14.png'),
+  ];
+  const nextP4Image = () => {
+    setCurrentIndex6((prevIndex) => (prevIndex === p4_images.length - 1 ? 0 : prevIndex + 1));
+  };
 
     return (
       <>
@@ -474,7 +498,124 @@ function Projects() {
           </h1>
         </div>
         
-      
+        <div className = "p4-tab-container">
+          <h1 className = "p2-caption">
+            Project 4: Trivia Game
+          </h1>
+        </div>
+
+        <div className = "p4-tab-layout">
+          <div className = "p4-credits-container">
+            <h1 className = "presentation-caption">
+              Credits
+            </h1>
+          </div>
+
+          <div className = "p4-full-credits-container">
+            <h1 className = "p4-explanation-text">
+              <h1 className = "p1-credits-role">Engineers:</h1> 
+              <h1 className = "my-name">Julianne Lee</h1>
+              Aneesh Sanghai
+              <p/>
+              Unnati Goyal
+              <p/>
+              Vaibhav Rungta
+              <p/>
+              Will Wei 
+              <p/>
+              Yashvi Jaju 
+
+
+            </h1> 
+          </div>
+
+          <div className = "p4-my-role-container">
+            <h1 className = "p4-explanation-text">
+              <h1 className = "p1-credits-role">Engineer:</h1> 
+              <h1 className = "my-name">Julianne Lee</h1>
+              My role as an engineer was to code the backend portion of the
+              trivia game. I was also in charge of making the UI for the game and the 
+              frontend part of the website. Some UI changes I made were adding 
+              color to indicate what questions players answered correct or wrong. 
+              I also styled the UI buttons. 
+            </h1> 
+          </div>
+          
+
+          <div className = "p4-presentation-role-container">
+            <h1 className = "presentation-caption">
+              My Role
+            </h1>
+          </div>  
+
+          <div className = "p4-presentation-caption-container">
+            <h1 className = "presentation-caption">
+              Presentation
+            </h1>
+          </div>
+          <button className = 'p4-next-button' onClick= {nextP4Image}>
+            <img src = {next_icon} className = "next-icon" alt = ""/>
+          </button>
+          <div className = "presentation-container">
+            <img src = {p4_images[currentIndex6]} className = "p4-image" alt = ""/>
+          </div>
+
+          <div className = "p4-explanation-box">
+            <h1 className = "p4-explanation-text">
+              Trivia Game is a react js, web game where players have to answer
+              as many correct questions as they can before time runs out. There are 
+              two modes on the website: singleplayer and multiplayer. Singleplayer mode
+              is for practicing, while multiplayer uses MongoDB to allow two players
+              to compete against each other. Players can save their score if they make
+              an account.
+            </h1> 
+          </div>
+
+          <div className = "p4-demo-container">
+            <div className = "p4-demo-box">
+              <h1 className = "p1-demo-caption">
+                Demo Video
+              </h1>
+            </div>
+            <video src={p4_video} className = "p4-demo-video-box" controls="controls" autoplay= {false} />
+
+            <div className = "p4-resources-box">
+              <h1 className = "p1-demo-caption">
+                Resources
+              </h1>
+            </div>
+
+            
+
+            <h1 className = "p4-resources-text">
+              <h1 className = "p1-credits-role">Repository:</h1> 
+              <p/>
+              <div className= "resource-link">
+                <a href="https://github.com/yashvijaju/trivia-game">
+                  https://github.com/yashvijaju/trivia-game
+                </a>
+              </div>
+              <p/>
+              <div className = "documentation-box">
+                <h1 className = "p1-credits-role">Documentation:</h1> 
+              </div>
+              <div className = "technical-button-align">
+                <a href="/Technical Specifications.pdf" download className= "documentation-button">
+                  Technical Specification
+                </a>
+              </div>
+
+              <div className = "design-button-align">
+                <a href="/Design.pdf" download className= "documentation-button">
+                  Design Documentation
+                </a>
+              </div>
+
+              
+             
+            </h1>
+          </div>
+        </div>
 
 
 
