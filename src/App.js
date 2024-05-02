83790c1e6171f2263a20f3ad48bd380d90fe9f66
julianne lee <jlee3932@usc.edu>
2024-05-01 22:27:33 -0700

    trying to fix
    trying to fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouterrRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/pages/home/Home';
 import Projects from './components/pages/projects/Projects';
