3fe3d911db3dcd12ff0ce0ebab6c4f5696ca3f14
julianne lee <jlee3932@usc.edu>
2024-05-03 21:56:52 -0700

    trying to deploy
    trying to deploy

--- src/components/pages/arcade/MonsterGame.js
+++ src/components/pages/arcade/MonsterGame.js
@@ -1,15 +1,16 @@
 import '../../../App';
 import './MonsterGame.css';
+// import '../../../../monster.framework.js'
 import React from 'react';
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function MonsterGame() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/MonsterGameAssets/monster.loader.js",
-    dataUrl: "/MonsterGameAssets/monster.data",
-    frameworkUrl: "/MonsterGameAssets/monster.framework.js",
-    codeUrl: "/MonsterGameAssets/monster.wasm",
-    streamingAssetsUrl: "/MonsterGameAssets/StreamingAssets",
+    loaderUrl: "../../../../build.loader.js",
+    dataUrl: "../../../../build.data",
+    frameworkUrl: "../../../../build.framework.js",
+    codeUrl: "../../../../build.wasm",
+    streamingAssetsUrl: "../../../../StreamingAssets",
   });
   return (
     <>
